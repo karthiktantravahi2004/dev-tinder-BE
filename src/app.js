1462c3df7f6b179a1b317095a5129f1d0e9edb3f
karthik <22p31a05c6@acet.ac.in>
2026-01-04 23:42:31 +0530

    04-01-2026

--- src/app.js
+++ src/app.js
@@ -2,13 +2,43 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("kill bill");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("handler 1");
 
-app.use("/test", (req, res) => {
-  res.send("hello");
-});
+    next();
+  },
+  (req, res, next) => {
+    console.log("handler 1");
+
+    next();
+  },
+  (req, res, next) => {
+    console.log("handler 2");
+
+    next();
+  },
+  (req, res, next) => {
+    console.log("handler 4");
+
+    next();
+  },
+  (req, res, next) => {
+    console.log("handler 5");
+
+    next();
+  },
+  (req, res, next) => {
+    console.log("handler 6");
+
+    next();
+  },
+  (req, res) => {
+    console.log("handler 7");
+    res.send("response!!");
+  }
+);
 
 app.listen(3001, () => {
   console.log("server port number 3000 Running......");
